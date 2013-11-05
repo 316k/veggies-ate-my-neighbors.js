ef2f41bc08c624068187325ddc4939cd3cf761ed
Nicolas Hurtubise <316k@legtux.org>
2013-11-05 18:17:47 -0500

    Creates a new Java/Netbeans project

---
+++ VeggiesAteMyNeighbors/src/ca/qc/bdeb/inf203/VeggiesAteMyNeighbors.java
@@ -0,0 +1,15 @@
+package ca.qc.bdeb.inf203;
+
+/**
+ *
+ * @author Nicolas Hurtubise
+ */
+public class VeggiesAteMyNeighbors {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

567d48f2afbea5afac4f992b53014abe6d8fa604
Nicolas Hurtubise <316k@legtux.org>
2013-12-09 14:30:18 -0500

    Revert "Removes the atomic rose from the default plants"
    This reverts commit 7a5dd1c68e62603ffa69a7ac52c82b47b2dc2696.

--- src/ca/qc/bdeb/inf203/model/Joueur.java
+++ src/ca/qc/bdeb/inf203/model/Joueur.java
@@ -50,6 +50,7 @@ public class Joueur {
     private Joueur() {
         items.add(new Item("pea-shooter", 0.001, 100, new Peashooter()));
         items.add(new Item("sunflower", 0.001, 50, new Sunflower()));
+        items.add(new Item("atomic-rose", 0.001, 50, new AtomicRose()));
         nbrSoleils = 250;
     }
     
